e1dd11accc264c9d4f0fc6738c9035a03b32a374
hongsaeroy <hjs971102@naver.com>
2024-06-10 17:48:03 +0900

    feat : proDetail page's Image Output

--- src/main/java/com/example/app/config/WebConfig.java
+++ src/main/java/com/example/app/config/WebConfig.java
@@ -7,16 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private String resourcePath = "/upload/**"; // view 에서 접근한 경로
-    // 윈도우의 경우
+    // 윈도우인 경우
     // private String savePath = "file:///C:/springboot_img/"; // 실제 파일 저장 경로
-    // 맥인 경우
-    private String savePath = "file:/Users/hongjaeseong/springboot_img/"; // 실제 파일 저장 경로
+    // 맥인 경우a
+    private String savePath = "file:///Users/hongjaeseong/springboot_img/"; // ProMainImg 실제 파일 저장 경로
+    private String savePath2 = "file:///Users/hongjaeseong/springboot_subImg/"; // ProSubImg 실제 파일 저장 경로
 
     // view 에서 접근한 경로를 spring 이 실제 파일 저장 경로에서 찾아준다
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //WebMvcConfigurer.super.addResourceHandlers(registry);
         registry.addResourceHandler(resourcePath)
-                .addResourceLocations(savePath);
+                .addResourceLocations(savePath)
+                .addResourceLocations(savePath2);
     }
 }
--- src/main/java/com/example/app/controller/ProjectDetailController.java
+++ src/main/java/com/example/app/controller/ProjectDetailController.java
@@ -1,8 +1,9 @@
 package com.example.app.controller;
 
+import com.example.app.domain.dto.ProjectDto;
 import com.example.app.domain.entity.Project;
-import com.example.app.domain.entity.ProjectFile;
 import com.example.app.domain.repository.ProjectRepository;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -23,23 +23,36 @@ import java.util.Optional;
 @RequestMapping("/th/project")
 public class ProjectDetailController {
 
+    // 프로젝트 경로 (추후 변경 가능성 있음)
+    String UPLOAD_PATH = "http://localhost:8080/upload/";
+
     @Autowired
     ProjectRepository projectRepository;
 
+    // 프로젝트 상세페이지에 데이터 전송 API
     @GetMapping("/project/{proCode}")
     String project(@PathVariable("proCode") String proCode, Model model) {
-        System.out.println(proCode);
-        Integer projectCode = Integer.parseInt(proCode);
-        Project project = projectRepository.findByProCode(projectCode);
-        List<ProjectFile> projectFileList = project.getProjectFileList();
-        System.out.println(project);
-        System.out.println(projectFileList);
-        // project 객체 null, 빈 값 체크
+        System.out.println(proCode); // 해당 프로젝트 proCode 확인
+        Integer projectCode = Integer.parseInt(proCode); // proCode 'int' 형 변환
+        Project project = projectRepository.findByProCode(projectCode); // 해당 proCode 의 project 엔터티 행 찾기 및 저장
+        ProjectDto projectDto = ProjectDto.toProjectDto(project); // Entity -> Dto
 
-        model.addAttribute("Project",project);
-        model.addAttribute("image1",projectFileList.get(0).getStoredFileName());
-        model.addAttribute("image2",projectFileList.get(1).getStoredFileName());
-        model.addAttribute("image3",projectFileList.get(2).getStoredFileName());
+        // ProMainImg 를 리스트에 담기 (3개 고정)
+        List<String> storedFileName = projectDto.getStoredFileName();
+        // ProMainImg 리스트 -> model 에 담기
+        model.addAttribute("Project", projectDto);
+        model.addAttribute("image1", UPLOAD_PATH + storedFileName.get(0)); // String 클래스자료형으로 데이터 전송
+        model.addAttribute("image2", UPLOAD_PATH + storedFileName.get(1));
+        model.addAttribute("image3", UPLOAD_PATH + storedFileName.get(2));
+
+        // ProSubImg 를 리스트에 담기 (5개 고정)
+        List<String> subStoredFileName = projectDto.getSubStoredFileName();
+        // ProSubImg 리스트 -> model 에 담기
+        model.addAttribute("subImage1", UPLOAD_PATH + subStoredFileName.get(0));
+        model.addAttribute("subImage2", UPLOAD_PATH + subStoredFileName.get(1));
+        model.addAttribute("subImage3", UPLOAD_PATH + subStoredFileName.get(2));
+        model.addAttribute("subImage4", UPLOAD_PATH + subStoredFileName.get(3));
+        model.addAttribute("subImage5", UPLOAD_PATH + subStoredFileName.get(4));
 
         return "th/project/project.html";
     }
