f39e66c08e8d9e16e0cf865643d9656f29f9b0ad
okeunjin <dhr2102@naver.com>
2024-06-17 22:05:58 +0900

    feat : show project information on payment page

--- src/main/java/com/example/app/controller/OrderController/OrderDetailController.java
+++ src/main/java/com/example/app/controller/OrderController/OrderDetailController.java
@@ -1,54 +1,43 @@
 package com.example.app.controller.OrderController;
 
 import com.example.app.domain.dto.OrderDto;
+import com.example.app.domain.dto.ProjectDto;
+import com.example.app.domain.entity.Project;
 import com.example.app.domain.repository.ProjectRepository;
 import com.example.app.domain.service.order.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @Slf4j
 @RequestMapping("/th/payment")
 public class OrderDetailController {
 
+    // 프로젝트 경로 (추후 변경 가능성 있음)
+    private final String UPLOAD_PATH = "http://localhost:8080/upload/";
     @Autowired
     private OrderService orderService;
-
     @Autowired
     ProjectRepository projectRepository;
 
-//    @GetMapping("/payment/{proCode}")
-//    String project(@PathVariable("proCode") String proCode, Model model) {
-//        System.out.println(proCode); // 해당 프로젝트 proCode 확인
-//        Integer projectCode = Integer.parseInt(proCode); // proCode 'int' 형 변환
-//        Project project = projectRepository.findByProCode(projectCode); // 해당 proCode 의 project 엔터티 행 찾기 및 저장
-//        ProjectDto projectDto = ProjectDto.toProjectDto(project); // Entity -> Dto
+    @GetMapping("/payment/{proCode}")
+    String payment(@PathVariable("proCode") String proCode, Model model) {
+        log.info("payment() proCode : " + proCode); // 해당 프로젝트 proCode 확인
+        Integer projectCode = Integer.parseInt(proCode); // proCode 'int' 형 변환
+        Project project = projectRepository.findByProCode(projectCode); // 해당 proCode 의 project 엔터티 행 찾기 및 저장
+        ProjectDto projectDto = ProjectDto.toProjectDto(project); // Entity -> Dto
 
-//        // ProMainImg 를 리스트에 담기 (3개 고정)
-//        List<String> storedFileName = projectDto.getStoredFileName();
-//        // ProMainImg 리스트 -> model 에 담기
-//        model.addAttribute("Project", projectDto);
-//        for (int i = 0; i < storedFileName.size(); i++) {
-//            model.addAttribute("image"+(i+1), UPLOAD_PATH + storedFileName.get(i));
-//        }
-//
-//        // ProSubImg 를 리스트에 담기 (5개 고정)
-//        List<String> subStoredFileName = projectDto.getSubStoredFileName();
-//        // ProSubImg 리스트 -> model 에 담기
-//        for (int i = 0; i < subStoredFileName.size(); i++) {
-//            model.addAttribute("subImage" + (i + 1), UPLOAD_PATH + subStoredFileName.get(i));
-//        }
-//        return "th/project/project.html";
-//        return "th/payment/payment.html";
-//    }
+        // ProMainImg 를 리스트에 담기 (3개 고정)
+        List<String> storedFileName = projectDto.getStoredFileName();
+        model.addAttribute("Project", projectDto);
+        model.addAttribute("mainImg", UPLOAD_PATH + storedFileName.get(0));
 
-
-    // 주문 페이지로 이동(GET)
-    @GetMapping("/payment")
-    public String showPaymentForm() {
-        return "th/payment/payment";
+        return "th/payment/payment.html";
     }
 
     // 주문 처리(POST)
