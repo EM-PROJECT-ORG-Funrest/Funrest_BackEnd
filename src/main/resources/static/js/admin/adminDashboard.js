2f5742a8188c841be750f6140d33f6ca26f45b73
hongsaeroy <hjs971102@naver.com>
2024-06-20 11:28:50 +0900

    feat : limit adminDashboard chart data's count

--- src/main/resources/static/js/admin/adminDashboard.js
+++ src/main/resources/static/js/admin/adminDashboard.js
@@ -4,15 +4,17 @@ const ctx = document.getElementById('myChart').getContext('2d');
 fetch('/api/visitData')
     .then(response => response.json())
     .then(data => {
-        const labels = data.map(item => item.visitDate);
-        const values = data.map(item => item.visitNum);
+        // 최근 30개의 데이터만 표시
+        const recentData = data.slice(-30);
+        const labels = recentData.map(item => item.visitDate);
+        const values = recentData.map(item => item.visitNum);
 
         new Chart(ctx, {
             type: 'line',               // 차트의 종류
             data: {                     // 차트의 데이터
                 labels: labels,
                 datasets: [{            // 실제 차트에 표시할 데이터들(Array), dataset 객체들을 담고 있음
-                    label: 'Number of Visits',          // dataset의 이름(String)
+                    label: '일자별 방문자 수',          // dataset의 이름(String)
                     data: values,                       // dataset의 값(Array)
                     borderColor: 'rgba(75, 192, 192, 1)',       // dataset의 선 색(rgba값을 String으로 표현)
                     borderWidth: 1      // dataset의 선 두께(Number)
