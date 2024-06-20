59e41a9ddd8124f173a6340a38018a78293b5533
hongsaeroy <hjs971102@naver.com>
2024-06-20 10:56:35 +0900

    feat : modify adminPage pagination's item numbering

--- src/main/java/com/example/app/controller/admin/AdminController.java
+++ src/main/java/com/example/app/controller/admin/AdminController.java
@@ -51,7 +51,12 @@ public class AdminController {
         // 2. 회원 관리 게시판 페이지 버튼
         int startPage = Math.max(1, userDtoPage.getPageable().getPageNumber() -4);
         int endPage = Math.min(userDtoPage.getPageable().getPageNumber() + 4, userDtoPage.getTotalPages());
+        // 3. 페이지를 넘겨도 번호를 계속 증가하기 위한 현재 페이지 번호 및 페이지 당 항목 수 저장
+        int currentPage = userDtoPage.getPageable().getPageNumber(); // 0 부터 시작
+        int pageSize = size;
         // 3. 모델 속성에 정보 저장
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("pageSize", pageSize);
         model.addAttribute("userDtoPages", userDtoPage);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
@@ -76,7 +81,14 @@ public class AdminController {
         // 4. 승인 프로젝트 게시판 페이지 버튼
         int permitStartPage = Math.max(1, projectDtoPageAfterList.getPageable().getPageNumber() -4);
         int permitEndPage = Math.min(projectDtoPageAfterList.getPageable().getPageNumber() + 4, projectDtoPageAfterList.getTotalPages());
-        // 3. 모델 속성에 정보 저장
+        // 5. 페이지를 넘겨도 번호를 계속 증가하기 위한 현재 페이지 번호 및 페이지 당 항목 수 저장
+        int beforeCurrentPage = projectDtoPageBeforeList.getPageable().getPageNumber(); // 0 부터 시작
+        int afterCurrentPage = projectDtoPageAfterList.getPageable().getPageNumber();
+        int pageSize = size;
+        // 6. 모델 속성에 정보 저장
+        model.addAttribute("beforeCurrentPage", beforeCurrentPage);
+        model.addAttribute("afterCurrentPage", afterCurrentPage);
+        model.addAttribute("pageSize", pageSize);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("projectBeforeList", projectDtoPageBeforeList);
