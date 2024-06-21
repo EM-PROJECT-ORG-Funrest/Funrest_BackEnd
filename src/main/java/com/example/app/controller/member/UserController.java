d7582da6f27dc09e102ee422a488b2e95442c7c5
rbghr <rbghrla@naver.com>
2024-06-21 09:31:49 +0900

    refact : UserController & UserServiceImpl code's refactoring

--- src/main/java/com/example/app/controller/member/UserController.java
+++ src/main/java/com/example/app/controller/member/UserController.java
@@ -52,6 +52,7 @@ public class UserController {
     @PostMapping("/member/findIdCheck")
     public ResponseEntity< Map<String, String>> findIdCheck(UserDto userDto){
         String userId = userDto.getUserId();
+        // 아이디가 존재하는지 여부 반환
         ResponseEntity< Map<String, String>> responseEntity =   userService.findIdCheck(userId);
         return responseEntity;
     }
@@ -64,12 +65,16 @@ public class UserController {
 
     @PostMapping("/member/findPwCheck")
     public ResponseEntity< Map<String, String>> finPwCheck(UserDto userDto,HttpSession session){
+        // findPw에서 입력한 userId 가져오기
         String userId = userDto.getUserId();
+        //랜덤 값 가져오기
         String randomValue = userService.randomValue();
 
+        // 세션에 randomValue, userId 저장 나중에 유효성 검사 및 pw변경 시 userId 사용
         session.setAttribute("userId", userId);
         session.setAttribute("randomValue", randomValue);
         try {
+            //여기서 가입정보 확인 가입된 사용자인지 아닌지 확인함 randomValue 는 메일전송 시 필요해서 같이 보냄
             ResponseEntity< Map<String, String>> responseEntity =   userService.findIdCheck2(userId, randomValue);
             return responseEntity;
         } catch (Exception e) {
@@ -79,12 +84,16 @@ public class UserController {
 
     @GetMapping("/member/findPwUrl/{randomValue}")
     public String findPwUrl(HttpServletRequest request, @PathVariable String randomValue) {
+        // 세션으로 randomValue 가져오기 유효성 검사를 위해
         HttpSession session = request.getSession();
         String storedRandomValue = (String) session.getAttribute("randomValue");
 
+        // 여기서 검사 url 의 randomValue와 저장된 randomValue 가 같은지 다른지 검사
         if (!randomValue.equals(storedRandomValue)) {
+            // 다르면 error 페이지로 전송
             return "th/member/error.html";
         } else {
+            // 같으면 findPwUrl 페이지로 전송
             return "/th/member/findPwUrl";
         }
     }
@@ -97,20 +106,23 @@ public class UserController {
 
     @PostMapping("/member/findSetPw")
     public String findSetPw (HttpServletRequest request, UserDto userDto){
-
+        // pw값 변경 시 유저 정보 확인을 위한 세션에 userId 가져오기
         HttpSession session = request.getSession();
         String userId = (String) session.getAttribute("userId");
 
+        // fundOwUrl에서 전송한 pw 값 을 가져옴
         String userPw = userDto.getUserPw();
-        log.info("/member/findSetPw invoke..." + userId + userPw);
+        // 암호화 해주고
         String userPwEncode = passwordEncoder.encode(userPw);
 
         // 비밀번호 재설정
-        //  ddd
         userService.findById(userId, userPwEncode);
-
+        
+        // 모든 로직 종료되었으니 세션값 삭제해주기
         session.removeAttribute("userId");
         session.removeAttribute("randomValue");
-        return "redirect:/th/main/main";
+        
+        //로그인 페이지로 리다이렉트 해주기
+        return "redirect:/th/member/login";
     }
 }
--- src/main/java/com/example/app/domain/service/member/UserServiceImpl.java
+++ src/main/java/com/example/app/domain/service/member/UserServiceImpl.java
@@ -22,37 +22,49 @@ public class UserServiceImpl {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    // 가입된 ID인지 찾아주는 기능 : findId 에서 사용
     public ResponseEntity<Map<String, String>> findIdCheck(String userId) {
+        // userId로 userEntity 가져와서
         Optional<User> user = userRepository.findByUserId(userId);
         Map<String, String> response = new HashMap<>();
 
+
         if (user.isEmpty()) {
+            //만약 비어있으면 전송
             response.put("message", "가입된 정보가 없습니다!");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         } else {
+            // 안비어있으면 전송
             response.put("message", "이미 가입된 이메일입니다!");
             return ResponseEntity.ok(response);
         }
     }
 
+    // 이메일 전송을 위한 userId 유효성 검사
     public ResponseEntity<Map<String, String>> findIdCheck2(String userId, String randomValue) throws Exception {
+
+        // userId로 유저 정보 가져오기
         Optional<User> user = userRepository.findByUserId(userId);
         Map<String, String> response = new HashMap<>();
 
         if (user.isEmpty()) {
+            // userEntity가 비어있으면 전송
             response.put("message", "가입된 정보가 없습니다!");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         } else {
+            // userEntity가 비어있지않으면 메일 전송
             response.put("message", "이메일을 확인해 주세요!");
             sendEmail(userId, randomValue);
             return ResponseEntity.ok(response);
         }
     }
 
-
+    
+    //랜덤값 만들어주기
     public String randomValue(){
         Random random = new Random();
-
+        
+        //총 58자리 랜던값 생성
         List<String> list = new ArrayList<>();
         for(int i=0; i<30; i++) {
             list.add(String.valueOf(random.nextInt(10)));
@@ -61,8 +73,10 @@ public class UserServiceImpl {
             list.add(String.valueOf((char)(random.nextInt(26)+65)));
         }
 
+        //list에 저장된 랜덤값배열 무작위로 섞기
         Collections.shuffle(list);
 
+        // list에 배열형태로 저장된 랜덤값을 String으로 반환 해주기
         StringBuilder sb = new StringBuilder();
         for (String item : list){
             sb.append(item);
@@ -71,22 +85,28 @@ public class UserServiceImpl {
         return randomValue;
     }
 
+    //메일 전송 서비스
     public void sendEmail(String userId, String randomValue) throws Exception{
+        // 메일 전송 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(userId);
         message.setSubject("[펀레스트] 비밀번호 찾기 인증 이메일입니다.");
         message.setText("다음 링크를 클릭해 비밀번호를 변경 완료해 주세요.\n");
+        // url에 랜덤값 붙여서 전송
         message.setText("http://localhost:8080/th/member/findPwUrl/"+randomValue);
 
         javaMailSender.send(message);
     }
 
+    // 재설정된 userPw 저장해주기
     public void findById(String userId, String userPwEncode){
+        // Optional<User> 값 그냥 userEntity로 변환 해주는 방법
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
+        // 받아온 인코딩된 userPw userEntity에 저장 
         user.setUserPw(userPwEncode);
-
+        // 저장
         userRepository.save(user);
 
     }
